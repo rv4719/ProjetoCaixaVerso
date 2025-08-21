8a1ad5543ad2e4d74d5913760b63d93f870069ff
Rosa Vasconcelos <161225438+rv4719@users.noreply.github.com>
2025-08-21 17:26:54 -0300

    Update GetTransactions.java

--- Projeto_BankAccount/src/main/java/ada/tech/lms/screen/GetTransactions.java
+++ Projeto_BankAccount/src/main/java/ada/tech/lms/screen/GetTransactions.java
@@ -29,23 +29,13 @@ public class GetTransactions implements ExecutedOption{
     @Override
     public void execute() throws IOException {
         BankAccount account = bankService.findAccountByUser(userAccount);
-//        Path dir = Path.of(".\\src\\transactions_statements");
-//        Files.createDirectories(dir);
-//        Path path = dir.resolve(account.getOwner().getCpf()+".txt");
+
         System.out.println("Cliente: "+account.getOwner().getName());
         System.out.println("Número da Conta: "+account.getAccountNumber());
         account.listaTransaction.sort(Comparator.comparing(Transaction::getData).reversed());
         account.listaTransaction.forEach(System.out::println);
 
-//        try (Stream<String> transactions = Files.lines(path, StandardCharsets.UTF_8)){
-//            transactions.forEach(System.out::println);
-//        };
 
-//        for (Transaction transaction:account.listaTransaction){
-//            System.out.println("Data: "+transaction.getData());
-//            System.out.println("Tipo de transação: "+transaction.getType());
-//            System.out.println("Valor: "+transaction.getAmount());
-//            System.out.println("Saldo após a operação: "+account.getBalance());
-//        }
     }
 }
+
