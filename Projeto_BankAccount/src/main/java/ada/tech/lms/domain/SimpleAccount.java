17d4412087e30618c810d2afd6c49b9e2b2b0db7
Rosa Vasconcelos <161225438+rv4719@users.noreply.github.com>
2025-08-21 17:24:41 -0300

    Update SimpleAccount.java
    limpeza do código

--- Projeto_BankAccount/src/main/java/ada/tech/lms/domain/SimpleAccount.java
+++ Projeto_BankAccount/src/main/java/ada/tech/lms/domain/SimpleAccount.java
@@ -21,26 +21,17 @@ public class SimpleAccount extends BankAccount {
             Path dir = Path.of(".\\src\\transactions_statements");
             Files.createDirectories(dir);
             Path path = dir.resolve(this.getOwner().getCpf()+".txt");
-//        Path path = dir.resolve("teste"+".txt");
+
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
             String dataFormatada = LocalDateTime.now().format(formatter);
 
             this.listaTransaction.add(new Transaction(LocalDateTime.now(),"DEBIT",-amount,this.getBalance()));
-
-//            Files.writeString(path,dataFormatada+"\n", StandardCharsets.UTF_8, StandardOpenOption.CREATE,StandardOpenOption.APPEND);
-//            Files.writeString(path,"DEBIT\n", StandardCharsets.UTF_8, StandardOpenOption.CREATE,StandardOpenOption.APPEND);
-//            Files.writeString(path, "R$ "+String.valueOf(-amount)+"\n", StandardCharsets.UTF_8, StandardOpenOption.CREATE,StandardOpenOption.APPEND);
-//            Files.writeString(path, "Saldo após a operação: R$"+String.valueOf(this.getBalance())+"\n", StandardCharsets.UTF_8, StandardOpenOption.CREATE,StandardOpenOption.APPEND);
-//            Files.writeString(path, String.format(
-//                    "Data/hora: %s - Valor: R$ %.2f - Transação: DEBIT - Saldo: R$ %.2f \n",
-//                    dataFormatada, -amount, this.getBalance(), StandardCharsets.UTF_8, StandardOpenOption.CREATE,StandardOpenOption.APPEND));
 
 
             Files.writeString(path, String.format(
                     "Data/hora: %s - Valor: R$ %.2f - Transação: DEBIT - Saldo: R$ %.2f - Número da conta: %s  - Cliente: %s \n",
                     dataFormatada, -amount, this.getBalance(),this.accountNumber,this.owner.getName()), StandardCharsets.UTF_8, StandardOpenOption.CREATE,StandardOpenOption.APPEND);
 
-//            this.listaTransaction.add(new Transaction(LocalDateTime.now(),"DEBIT",-amount,this.getBalance()));
         } else {
             throw new IllegalArgumentException("Insufficient balance.");
         }
